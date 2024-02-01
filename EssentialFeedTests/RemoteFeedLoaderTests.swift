8958532e794f0044b35f0389b1b9a96ba9f78d29
Gabriel Mors <gabrielmors210@gmail.com>
2024-02-01 06:08:09 -0400

    solicite dados da URL no carregamento

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,20 +8,35 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.share.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let share = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
-    func test_init_doesNotRequestDataFromURLTest() {
-        let client = HTTPClient()
+    func tests_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.share
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func tests_load_requesteDataFromURL() {
+        let client = HTTPClient.share
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
