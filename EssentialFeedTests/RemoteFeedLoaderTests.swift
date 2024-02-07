4b550d96ad02d883f4dd191d6d82196811fb81fc
Gabriel Mors <gabrielmors210@gmail.com>
2024-02-06 23:32:33 -0400

    fazendo test para evitar varias chamadas da api

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -25,18 +25,29 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func tests_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
         return (sut, client)
-        
     }
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
     
